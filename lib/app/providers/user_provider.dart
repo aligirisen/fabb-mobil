83f2419d0b27876a3a17f80de734b9e7208c90c5
Ali <aliriza.girisen@gmail.com>
2023-03-09 19:28:12 +0300

    post register method

--- lib/app/providers/user_provider.dart
+++ lib/app/providers/user_provider.dart
@@ -12,7 +12,115 @@ class user_provider {
       final jsonData = json.decode(response.body);
       print(jsonData);
     } else {
-      throw Exception('Failed to load user data from API');
+      throw Exception(
+          'Failed to load user data from API: ${response.statusCode}');
+    }
+  }
+
+  void postRegister() async {
+    String url = "https://192.168.1.106:5111/user/signup";
+
+    // Define the headers
+    Map<String, String> headers = {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    };
+
+    // Define the body
+    Map<String, dynamic> body = {
+      "email": "mobil@gmail.com",
+      "password": "deneme",
+      "phone_number": "+905486214587",
+      "full_name": "Ali ali"
+    };
+
+    // Send the POST request
+    http.Response response = await http.post(Uri.parse(url),
+        headers: headers, body: jsonEncode(body));
+
+    // Handle the response
+    if (response.statusCode == 200) {
+      // Success
+      print("Data Posted Successfully!");
+    } else {
+      // Failure
+      print("Failed to Post Data. Error: ${response.statusCode}");
     }
   }
 }
+
+
+/* Yukarıdaki get methodu çalışır halde.
+
+
+controller'la bağlanacak
+
+
+class MyController extends GetxController {
+  
+  RxString name = ''.obs;
+  RxString email = ''.obs;
+  RxBool isLoading = false.obs;
+  
+  void setName(String value) {
+    name.value = value;
+  }
+  
+  void setEmail(String value) {
+    email.value = value;
+  }
+  
+  void postData() async {
+    isLoading.value = true;
+    
+    // Define the URL, headers, and body as before
+    
+    // Send the POST request
+    http.Response response = await http.post(Uri.parse(url), headers: headers, body: jsonEncode(body));
+    
+    isLoading.value = false;
+    
+    // Handle the response
+    if (response.statusCode == 200) {
+      // Success
+      print("Data Posted Successfully!");
+      name.value = '';
+      email.value = '';
+    } else {
+      // Failure
+      print("Failed to Post Data. Error: ${response.statusCode}");
+    }
+  }
+  
+  void resetForm() {
+    name.value = '';
+    email.value = '';
+  }
+  
+}
+
+
+
+
+
+
+
+
+TextField(
+  controller: TextEditingController(text: myController.name.value),
+  onChanged: (value) => myController.setName(value),
+  decoration: InputDecoration(
+    labelText: 'Name',
+    border: OutlineInputBorder(),
+  ),
+),
+
+
+
+
+
+
+
+
+
+*/
